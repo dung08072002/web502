e0bcb55f00e6c1f9f2ba0c15ca07d7b40210bcd5
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-03-12 13:39:41 +0700

    Chap3

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,22 @@
 "use strict";
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-const a = 20;
-const b = 10;
-const name = "dungnv"; //union
-const age = 20;
-const status = true;
-const info = { id: 1, name: 'a' };
-const myArr = ["a", "b", "c"];
-const numberArr = [1, 2, 3, 4];
-const objectArr = [{ id: 1, name: 'b' }, { id: 2, name: 'c' }];
-function sum(numA, numB) {
-    return numA + numB;
+function showData(a, b) {
+    return [a, b];
 }
-;
-const appDiv = (_a = document.querySelector('#app')) === null || _a === void 0 ? void 0 : _a.innerHTML;
-appDiv === "abc";
-console.log(name);
+showData(1, 2);
+showData("Viet", "Dung");
+const app = document.querySelector('#app').innerHTML;
+const product = [
+    { id: 1, name: "proA" },
+    { id: 2, name: "proB" },
+    { id: 3, name: "proC" },
+];
+function getProduct(p) {
+    return [p];
+}
+getProduct(product.map((item) => `
+    <h1>${item.name}</h1>
+`).join(""));
+
 //# sourceMappingURL=app.js.map
