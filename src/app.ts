3f6bc9df6668592cb2d1e293ca8900f0abb9b874
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-03-12 13:39:51 +0700

    Chap3

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,20 @@
-export{}
+export { }
+const app = document.querySelector('#app')?.innerHTML;
 
 type Product = {
-    id: number;
-    name: string;
-    role?: boolean;
+    id: number, name: string
 }
 
-const a: number = 20;
-const b: number = 10;
-const name: string | number = "dungnv"; //union
-const age: number = 20;
-const status: boolean = true;
-const info: Product = {id: 1, name: 'a'};
-const myArr: string[] = ["a","b","c"];
-const numberArr: number[] = [1,2,3,4];
-const objectArr: Product[] = [{id:1, name: 'b'}, {id:2, name: 'c'}];
+const products: Product[] = [
+    { id: 1, name: "proA" },
+    { id: 2, name: "proB" },
+    { id: 3, name: "proC" },
+]
 
-function sum(numA: number,numB: number): number{
-    return numA + numB;
-};
 
-const appDiv = document.querySelector('#app')?.innerHTML;
-appDiv === "abc";
-console.log(name);
+
+function showProduct<T extends Product[]>(products :T) {
+    const result = products.map(item => `
+        <h1>${item.name}</h1>
+    `)
+}
